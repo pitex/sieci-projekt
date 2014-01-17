111b34d45350d80499f7e10838efc39291b53a62
pitex <michalpiekarz09@gmail.com>
2014-01-17 23:13:57 +0100

    client stub no protocol yet

--- src/joinservice/client.go
+++ src/joinservice/client.go
@@ -1,5 +1,23 @@
 package joinservice
 
+import "net"
+
 type Client struct {
-	knownIp string
+	address		string
+	knownIp		string
+	capacity	int
+}
+
+// Connects client to network containing knownIp
+func (c *Client) Connect() error {
+	conn, err := net.Dial("tcp", c.knownIp) 
+	if err != nil {
+		return err
+	}
+
+	var request = string.Bytes("") //TODO define request message
+	n, err := conn.Write(request)
+
+	var response = make([]byte, 1024)
+	n, err := conn.Read(response)
 }
