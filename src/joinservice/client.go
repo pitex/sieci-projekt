36801fd1a406dcc197a60021af5b802bdf34b8e9
pitex <michalpiekarz09@gmail.com>
2014-01-19 23:19:54 +0100

    client sending message and receiving

--- src/joinservice/client.go
+++ src/joinservice/client.go
@@ -1,23 +1,32 @@
 package joinservice
 
-import "net"
-import "fmt"
-import "strings"
-import "../protocols/sip"
+import (
+	"net"
+	"fmt"
+	"strings"
+	"../protocols/sip"
+	"../protocols"
+)
 
 type Client struct {
 	Address		string
 	KnownIp		string
 	Capacity	int
+	IsRoot		bool
 }
 
-//	Connects client to network containing knownIp
-func (c *Client) Connect() error {
+func NewClient(myip string, capacity int, ip string, root bool) *Client {
+	return &Client{myip,ip,capacity,root}
+}
+
+//	Connects client to networkz
+func (c *Client) Connect() (string, error) {
 	//	Setup connection
 	conn, err := net.Dial("tcp", c.KnownIp) 
 	if err != nil {
-		return err
+		return "", err
 	}
+
 
 
 	//	Create request
@@ -25,13 +34,16 @@ func (c *Client) Connect() error {
 	request.Type = "FIND"
 	request.Data = fmt.Sprintf("IP=%s,CAP=%d", c.Address, c.Capacity)
 
+	byteRequest := []byte(request.ToString())
+
 	//	Send request
-	n, err := conn.Write(strings.Bytes(request.ToString()))
+	_, err = conn.Write(byteRequest)
 
 	//	Check if sending was successful
 	if err != nil {
-		return err
+		return "", err
 	}
+
 
 
 	//	Get response
@@ -40,11 +52,24 @@ func (c *Client) Connect() error {
 
 	//	Check if there was error with read
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	//	Save response as string
-	response := string(respBytes[:n])
+	//	Save response
+	response := strings.Split(string(respBytes[:n]), protocols.GetSep())
 
-	return nil
+
+
+	//	Get received data
+	data := strings.Split(response[1], ",")
+
+	//	Find server address
+	for _, s := range data {
+		kv := strings.Split(s, "=")
+		if (kv[0] == "IP") {
+			return kv[1], nil
+		}
+	}
+
+	return "", nil
 }
