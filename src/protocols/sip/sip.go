1c3d7760b47d1d6be1f556e1bedecd6b50f54737
pitex <michalpiekarz09@gmail.com>
2014-01-20 12:56:27 +0100

    Comments

--- src/protocols/sip/sip.go
+++ src/protocols/sip/sip.go
@@ -1,23 +1,25 @@
 package sip
 
 import "fmt"
+import "../"
 
-/*
-	possible message types:
-	INF - 'yes, I received your msg'
-
-	BLD - 'begin building tree chart' - msg sent to parent only
-	TRA - 'after you receive this message I will begin sending you parted file', sent from parent to child only
-
-	REQ - 'I received a request of a new machine' sent to parent only
-	FND - 'I am sending a message about who should be new machine's parent' sent do child only
-*/
+//	Message type used in Simple Information Protocol.
 type Message struct {
+	//	Type of the message, must be one of:
+	//	INF - 'yes, I received your msg'
+	// 
+	//	BLD - 'begin building tree chart' - msg sent to parent only
+	//	TRA - 'after you receive this message I will begin sending you parted file', sent from parent to child only
+	// 
+	//	REQ - 'I received a request of a new machine' sent to parent only
+	//	FND - 'I am sending a message about who should be new machine's parent' sent do child only
 	Type	string
+	//	Additional data for message given as pair KEY=VALUE separated with comma.
 	Data	string
 	Error	string
 }
 
+//	Returns string representation of the message.
 func (msg *Message) ToString() string {
-	return fmt.Sprintf("%s|%s|%s", msg.Type, msg.Data, msg.Error)
+	return fmt.Sprintf("%s%s%s%s%s", msg.Type, protocols.GetSep(), msg.Data, protocols.GetSep(), msg.Error)
 }
